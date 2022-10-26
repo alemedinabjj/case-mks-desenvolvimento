03e2302a7cfafcece661d45c2ecc04ee5d00bb23
Alexandre Medina <alesurf13@gmail.com>
2022-10-26 10:29:35 -0300

    feat favicon webpage

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -35,7 +35,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet" />
-          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+          <link rel="shortcut icon" href="/favicon.ico" type="image/icon" />
           <meta name="description" content="A simple project to case MKS Desenvolvimento" />
         </Head>
         <body>
